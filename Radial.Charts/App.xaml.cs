fc60c198106f616779ff022650752e485dc7b448
Jesús Iván Hernández Ramírez <Jesus_Hdez.Ramirez@outlook.com>
2020-08-13 10:40:09 -0500

    Commit

---
+++ Radial.Charts/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Radial.Charts
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Radial.Charts/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace Radial.Charts
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new Dictionary<string, double> { { "A", 8.167 }, { "E", 12.702 }, { "I", 6.966 }, { "O", 7.507 }, { "U", 2.758 } };
+        }
+    }
+}
